fd2ba152dcf2e54d19db7de91261f51ab5c001fa
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-30 23:14:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,7 +12,7 @@ def load_library(path)
 end
 def get_japanese_emoticon(path, emoticon)
   load_library(path).each do |key, value|
-    if value[:japanese]== emoticon
+    if value[:english]== emoticon
       return key
     end
   end
@@ -20,7 +20,7 @@ def get_japanese_emoticon(path, emoticon)
 end
 def get_english_meaning(path, emoticon)
   load_library(path).each do |key, value|
-    if value[:english]== emoticon
+    if value[:japanese]== emoticon
       return key
     end
   end
