c5699a2f2040981d05092bf21ec288a621021594
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-30 23:06:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(path, emoticon)
       return key
     end
   end
-  return "Sorry, emoticon not found"
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
