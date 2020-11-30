c70550a92e3db041b5af2e50f0e15a9642b064d7
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-30 23:06:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,9 @@ def get_japanese_emoticon(path, emoticon)
   load_library(path).each do |key, value|
     if value[:japanese]==emoticon
       return key
-end
+    end
+  end
+  return "Sorry, emoticon not found"
 end
 
 def get_english_meaning
