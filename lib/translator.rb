234983deecc1c79615d29c8a581edd5bd099d6a8
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-30 22:56:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 require 'pry'
 def load_library(path)
   final_hash={}
-  yaml.load_file(path).each do |key, value|
+  YAML.load_file(path).each do |key, value|
     final_hash[key]={}
     final_hash[key][:english]= value[0]
     final_hash[key][:japanese]=value[1]
